d6f3f3d7465d87bbcbc9eb40af2922645f3de8e0
[Suleymen Maulenuly] <suleymen2005gw@gmail.com>
2024-10-05 21:10:48 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        DVDPlayer dvdPlayer = new DVDPlayer();
-        Projector projector = new Projector();
-        SoundSystem soundSystem = new SoundSystem();
 
-        HomeTheaterFacade homeTheater = new HomeTheaterFacade(dvdPlayer, projector, soundSystem);
-
-        homeTheater.watchMovie("Inception");
-
-        System.out.println(" ");
-        homeTheater.endMovie();
     }
 }
